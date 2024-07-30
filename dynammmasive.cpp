d3f3bfc20b555c19c51ce356b8ee4bed510b6f9f
cowboyHat123111 <cowboy.hat1@protonmail.com>
2024-07-30 17:25:52 +0300

    update docs

---
+++ dynammmasive.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    SetConsoleOutputCP(1251);
+
+    cout << "Введіть розмірінсть масиву:" << endl;
+    int n;
+    cin >> n;
+    int *mas = new int[n];
+    cout << "Вивід масиву заповненого випадковим чином" << endl;
+    for (int i = 0;i < n;i++)
+    {
+        mas[i] = rand() % 85 + 15;
+        cout <<setw(3)<< mas[i];
+    }
+    cout << "\n";
+
+    delete[] mas;
+}
+
